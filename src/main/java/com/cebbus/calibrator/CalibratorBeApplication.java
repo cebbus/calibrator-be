6986e54ff3d508ede73e915f9ee905b5d8d77f6e
cebbus <cebbus@gmail.com>
2022-12-01 01:18:29 +0300

    feat: first commit

---
+++ src/main/java/com/cebbus/calibrator/CalibratorBeApplication.java
@@ -0,0 +1,13 @@
+package com.cebbus.calibrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalibratorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalibratorBeApplication.class, args);
+    }
+
+}
