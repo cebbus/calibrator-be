be8d6325d1d54afe96758d8e0cfd90b51a35665e
Sedat Pehlivanlı <sedadp@finecus.com>
2022-12-11 16:24:46 +0300

    fix: component hierarchy corrected

--- src/main/java/com/cebbus/calibrator/calculation/CFourPointFiveAnalysis.java
+++ src/main/java/com/cebbus/calibrator/calculation/CFourPointFiveAnalysis.java
@@ -12,7 +12,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
-public class CFourPointFiveAnalysis extends IdTreeAnalysis {
+public class CFourPointFiveAnalysis extends EntropyAnalysis {
 
     public CFourPointFiveAnalysis(
             StructureRepository structureRepository,
